afe854ee013d357766e68de521fedf8cbb5cf817
xsuto <michal.sutowicz@gmail.com>
2024-10-23 00:54:26 +0200

    Add vercel analytics and speed insights

--- src/app.tsx
+++ src/app.tsx
@@ -7,6 +7,11 @@ import { AppFooter } from "./components/app-footer";
 import { AppHeader } from "./components/app-header";
 import { ToastList, ToastRegion } from "./components/ui/toast";
 import { SocialMeta } from "./components/social-meta";
+import { inject } from "@vercel/analytics";
+import { injectSpeedInsights } from "@vercel/speed-insights"
+
+inject();
+injectSpeedInsights()
 
 export default function App() {
   return (
